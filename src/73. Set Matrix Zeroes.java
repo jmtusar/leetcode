a8957ff38f5fa99fc66b85391c504702cddc7393
Jobar Mahbob Tusar <50876337+jmtusar@users.noreply.github.com>
2025-03-05 16:27:12 +0600

    Create 73. Set Matrix Zeroes.java

---
+++ src/73. Set Matrix Zeroes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n= matrix[0].length;
+     
+        boolean[] rows = new boolean[m];
+        boolean[] column = new boolean[n];
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    rows[i]=true;
+                    column[j]=true;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(rows[i] || column[j])
+                {
+                    matrix[i][j]=0;            }
+            }
+        }
+
+
+    }
+}
